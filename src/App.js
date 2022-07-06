0eeb05e6373942e17180fbb2d08e230d3ee16f59
Ashford Lee Parmesar <ashfordlee19@gmail.com>
2022-07-06 10:28:45 +0200

    add styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
 function App() {
+
+	const createDigits = () => {
+		const digits =[];
+
+		for(let i = 1; i < 10; i++) {
+			digits.push(
+				<button key={i}>{i}</button>
+			)
+		}
+
+		return digits;
+	}
+
   return (
     <div className='App'>
 		<div className='calculator'> 
@@ -18,6 +31,7 @@ function App() {
 			</div>
 
 			<div className='digits'>
+				{createDigits()}
 				<button>0</button>
 				<button>.</button>
 				<button>=</button>
